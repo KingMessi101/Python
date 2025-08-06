1d875ba05ebb3416d7770ce5a2dddd9df9a31556
KingMessi101 <anishbanerjee1000@gmail.com>
2025-08-06 20:37:07 +0930

    Add files via upload

---
+++ Cancatenated Text.py
@@ -0,0 +1,7 @@
+s1= input("Enter First Sentence : ")
+s2= input("Enter Second Sentence : ")
+s3= input("Enter Third Sentence : ")
+
+result= s1 + " " + s2 + " " + s3
+
+print("concatenated text: ", result)
