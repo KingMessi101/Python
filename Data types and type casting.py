9525b9b740a0ec937bb097236994cd8db7d9c417
KingMessi101 <anishbanerjee1000@gmail.com>
2025-08-06 20:07:01 +0930

    Add files via upload

---
+++ Data types and type casting.py
@@ -0,0 +1,21 @@
+name= "Steave"
+age= 15
+is_student= True
+weight= 1234567890.5 
+
+print("name :", name)
+print("Data sype of Name is", type(name))
+print("age :", age)
+print("Data sype of Age is", type(age))
+print("is_student :", is_student)
+print("Data sype of is_student is", type(is_student))
+print("weight :", weight)
+print("Data sype of weight is", type(weight))
+
+print("\n After Type Casting....")
+age= str(age)
+print(age)
+print("Data Type of age is", type(age))
+age= int(weight)
+print(weight)
+print("Data Type of weight is", type(weight))
