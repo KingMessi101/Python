1e05b946a6e6c64f3ece0f96df3443f67fbc0a83
KingMessi101 <anishbanerjee1000@gmail.com>
2025-08-11 20:44:00 +0930

    Add files via upload

--- Polygon.py
+++ Polygon.py
@@ -4,21 +4,13 @@ turtle.Screen().bgcolor("orange")
 
 board= turtle.Turtle()
 
-board.forward(100)
-
-board.left(120)
-board.forward(100)
-
-board.left(120)
-board.forward(100)
-
-board.forward(100)
+board.forward(200)
 board.left(90)
 
 board.forward(100)
 board.left(90)
 
-board.forward(100)
+board.forward(200)
 board.left(90)
 
 board.forward(100)
