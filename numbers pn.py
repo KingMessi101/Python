0efd2b62ccbbf07bae2d418694034fa2f5e8b4c1
KingMessi101 <anishbanerjee1000@gmail.com>
2025-08-13 19:52:28 +0930

    Add files via upload

---
+++ numbers pn.py
@@ -0,0 +1,11 @@
+number= int(input("Enter Number To check"))
+print("number to be checked :", number)
+
+if number>0:
+ print("this is a positive number")
+
+elif number<0:
+ print("this is a negitive number")
+
+else:
+  print("thus is neutral")
