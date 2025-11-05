1450d437ee553c7bd3e1444f0240b355d84e58e1
KingMessi101 <anishbanerjee1000@gmail.com>
2025-11-05 21:30:55 +1030

    Add files via upload

--- Chat Bot.py
+++ Chat Bot.py
@@ -4,15 +4,7 @@ name = input()
 
 print(f"nice to meet you {name}")
 
-print("how are you feeling today (good/bad) : ")
+print("What is your favourite food: ")
 mood = input().lower()
 
-if mood == "good":
-    print("i'm happy to hear that")
-elif mood == "bad":
-    print("i'm sorry to hear that.")
-
-else:
-    print("i see. sometimes it is hard to put your feelings into words.")
-
-print(f"it was nice chatting with you {name}. bye")
+print(f"nice to know, it was nice chatting with you {name}. bye")
