db081e29e8b0942e30d4f107cf33a9d63a29c55a
KingMessi101 <anishbanerjee1000@gmail.com>
2025-12-03 21:19:52 +1030

    Add files via upload

--- Colour convertion and croping.py
+++ Colour convertion and croping.py
@@ -17,3 +17,13 @@ cropped_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 plt. imshow(cropped_rgb)
 plt. title("Cropped Region")
 plt. show()
+
+(h, w) = image.shape[:2]
+center = (w//2, h//2)
+M = cv2.getRotationMatrix2D(center, 45, 1.0)
+rotated = cv2.warpAffine(image, M, (w, h))
+
+rotated_rgb = cv2.cvtColor(rotated, cv2.COLOR_BGR2RGB)
+plt.imshow(rotated_rgb)
+plt.title("Rotated Image")
+plt.show()
